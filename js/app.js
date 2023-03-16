39a8f8612381d86485ac2d734a88a6b15131ff2f
markb <markbuchholz8@gmail.com>
2023-03-16 16:16:05 -0600

    50% tickets

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let tickets_sold=1000;
+let venue_capacity=2000;
+let performer="mark"
+let is_sold_out=false;
+if(is_sold_out === true){
+ console.log("All Sold Out")
+}else{
+    console.log("Tickets Are Still Available")
+}
+if(tickets_sold >=1800){
+    console.log("Almost Sold Out")
+}
+if(tickets_sold >=1000){
+    console.log("Tickets On Sale Now")
+}
