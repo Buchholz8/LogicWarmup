580686566fe7c048904e6d5c9efc8c91b961f781
markb <markbuchholz8@gmail.com>
2023-03-16 16:39:18 -0600

    repeat

--- js/app.js
+++ js/app.js
@@ -7,9 +7,23 @@ if(is_sold_out === true){
 }else{
     console.log("Tickets Are Still Available")
 }
-if(tickets_sold >=1800){
+if(tickets_sold / venue_capacity >=0.9){
     console.log("Almost Sold Out")
 }
-if(tickets_sold >=1000){
+if(tickets_sold / venue_capacity>=0.5){
+    console.log("tickets selling fast")
+}else{
     console.log("Tickets On Sale Now")
 }
+if(tickets_sold > venue_capacity){
+    console.log("error")
+}
+if(tickets_sold < venue_capacity === is_sold_out === true){
+    console.log("system error")
+}
+if(performer = "mark" === is_sold_out === false){
+    console.log("error in system")
+} else {
+    console.log("all good")
+}
+let is_repeat_show
